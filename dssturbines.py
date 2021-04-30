f4cac099abc8b640e76a58d3a63ce8b99f8c891f
Thomas <tjankovic12@gmail.com>
2021-04-30 16:05:41 -0400

    dssturbines.py will add a turbine per bus in a circuit

---
+++ dssturbines.py
@@ -0,0 +1,162 @@
+import windpowerlib
+import pvlib
+from windpowerlib import WindTurbine
+import urllib
+from feedinlib import WindPowerPlant, Photovoltaic, get_power_plant_data, era5
+import os
+import glob
+import xarray
+import pandas as pd
+import matplotlib.pyplot as plt
+import plotly.graph_objects as go
+import opendssdirect as dss
+import numpy as np
+from scipy.optimize import minimize 
+import warnings
+
+def runDssCommand(dsscmd):
+	from opendssdirect import run_command, Error
+	x = run_command(dsscmd)
+	latest_error = Error.Description()
+	if latest_error != '':
+		print('OpenDSS Error:', latest_error)
+	return x
+
+
+def runDss(dssString):
+    DSSNAME = 'lehigh.dss'
+    with open(DSSNAME,'w') as lehigh:
+        lehigh.write(dssString)
+    x = runDssCommand(f'Redirect "{DSSNAME}"')
+    return x
+
+
+def dssToTree(pathToDss):
+  ''' Convert a .dss file to an in-memory, OMF-compatible 'tree' object.
+	Note that we only support a VERY specifically-formatted DSS file.'''
+	# TODO: Consider removing the handling for 'wdg=' syntax within this block, as we will not support it in an input file. 
+	# Ingest file.
+  with open(pathToDss, 'r') as dssFile:
+    contents = dssFile.readlines()
+	# Lowercase everything. OpenDSS is case insensitive.
+  contents = [x.lower() for x in contents]
+	# Clean up the file.
+  for i, line in enumerate(contents):
+    # Remove whitespace.
+    contents[i] = line.strip()
+		# Comment removal
+    bangLoc = line.find('!')
+    if bangLoc != -1:
+      contents[i] = line[:bangLoc]
+    # Join using the tilde (~) syntax
+    if line.startswith('~'):
+      # Look back to find the first line with content.
+      for j in range(i - 1, 0, -1):
+        if contents[j] != '':
+          contents[j] = contents[j] + contents[i].replace('~', ' ')
+          contents[i] = ''
+          break
+	# Capture original line numbers and drop blanks
+  contents = dict([(c,x) for (c, x) in enumerate(contents) if x != ''])
+	# Lex it
+  convTbl = {'bus':'buses', 'conn':'conns', 'kv':'kvs', 'kva':'kvas', '%r':'%r'}
+  # convTbl = {'bus':'buses', 'conn':'conns', 'kv':'kvs', 'kva':'kvas', '%r':'%rs'} # TODO at some point this will need to happen; need to check what is affected i.e. viz, etc
+
+  from collections import OrderedDict 
+  for i, line in contents.items():
+    jpos = 0
+    if line.startswith('export'):
+      contents[i] = OrderedDict({'!CMD':'export ' + line[7:]})
+      continue
+    try:
+      contents[i] = line.split()
+      ob = OrderedDict() 
+      ob['!CMD'] = contents[i][0]
+      if len(contents[i]) > 1:
+        for j in range(1, len(contents[i])):
+          jpos = j
+          splitlen = len(contents[i][j].split('='))
+          k,v=('','',)
+        #   print(contents[i], splitlen)
+          if splitlen==3:
+            print('OMF does not support OpenDSS\'s \'file=\' syntax for defining property values.')
+            k,v,f = contents[i][j].split('=')
+						# replaceFileSyntax() # DEBUG
+						## replaceFileSyntax  should do the following:
+						# parse the filename (contained in v)
+						# read in the file and parse as array
+						# v = file content array, cast as a string
+          else:
+            k,v = contents[i][j].split('=')
+					# TODO: Should we pull the multiwinding transformer handling out of here and put it into dssFilePrep()?
+          if k == 'wdg':
+            continue
+          if (k in ob.keys()) or (convTbl.get(k,k) in ob.keys()): # if the single key already exists in the object, then this is the second pass. If pluralized key exists, then this is the 2+nth pass
+            # pluralize the key if needed, get the existing values, add the incoming value, place into ob, remove singular key
+            plurlk = convTbl.get(k, None) # use conversion table to pluralize the key or keep same key
+            incmngVal = v
+            xistngVals = []
+            if k in ob: # indicates 2nd winding, existing value is a string (in the case of %r, this indicates 3rd winding as well!)
+              if (type(ob[k]) != tuple) or (type(ob[k]) != list): # pluralized values can be defined as either
+							#if iter(type(ob[k])):
+                xistngVals.append(ob[k])
+                del ob[k]
+            if plurlk in ob: # indicates 3rd+ winding; existing values are tuples
+              for item in ob[plurlk]:
+                xistngVals.append(item)
+            xistngVals.append(incmngVal) # concatenate incoming value with the existing values
+            ob[plurlk] =  tuple(xistngVals) # convert all to tuple
+          else: # if single key has not already been added, add it
+            ob[k] = v
+    except:
+      raise Exception(f"Error encountered in group (space delimited) #{jpos+1} of line {i + 1}: {line}")
+			# raise Exception("Temp fix but error in loop at line 76")
+    if type(ob) is OrderedDict:
+      contents[i] = ob
+	# Print to file
+	#with open('dssTreeRepresentation.csv', 'w') as outFile:
+	#	ii = 1
+	#	for k,v in contents.items():
+	#		outFile.write(str(k) + '\n')
+	#		ii = ii + 1
+	#		for k2,v2 in v.items():
+	#			outFile.write(',' + str(k2) + ',' + str(v2) + '\n')
+  return list(contents.values())
+
+
+def treeToDss(treeObject, outputPath):
+  outFile = open(outputPath, 'w')
+  for ob in treeObject:
+    line = ob['!CMD']
+    for key in ob:
+      if not key.startswith('!'):
+        line = f"{line} {key}={ob[key]}"
+      if key.startswith('!TEST'):
+        line = f"{line} {ob['!TEST']}"
+        print(line)
+    outFile.write(line + '\n')
+  outFile.close()
+
+
+def addTurbine(dssTree):
+	# add a monitor at each load immediately before solve statement
+	for i in loads:
+	  t.insert(t.index([x for x in t if x.get('!CMD','').startswith('solve')][0]), {'!CMD': 'new',
+	  'object': f'monitor.{i}',
+	  'element': i,
+	  'terminal': '1'})
+
+	buses = [x.get('bus') for x in t if x.get('!CMD','').startswith('setbusxy')]
+
+	# add a wind turbine at each bus immediately before monitor definitions
+	for i in buses:
+	  for s in range(157):
+	    t.insert(t.index([x for x in t if x.get('object','').startswith('monitor.')][0]), {'!CMD': 'new',
+	    'object': f'generator.wind_{i}_{s}',
+	    'bus': f'{i}.1.2.3',
+	    'kva': '15.6',
+	    'pf': '1.0',
+	    'conn': 'delta',
+	    'duty': 'wind',
+	    'model': '1'})
+

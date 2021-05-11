ee1a4f4e24deeb72c79f514717c559e9a024c8eb
David Pinney <david@pinney.org>
2021-05-11 14:34:21 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,8 +8,8 @@ from setuptools.command.install import install
 from subprocess import check_call
 
 def pre_install():
-    check_call("pip install git+https://github.com/wind-python/windpowerlib".split())
-    check_call("pip install git+https://github.com/oemof/feedinlib".split())
+    check_call("pip install git+https://github.com/wind-python/windpowerlib.git@60f58ce76555b3ff33b31a959cd14958546b24e8".split())
+    check_call("pip install git+https://github.com/oemof/feedinlib.git@cd5d2392b398953e40a38909ea0f3a94986cb632".split())
 
 class PreDevelopCommand(develop):
     def run(self):

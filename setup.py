52720bd439b70b8b412e9bd26d718830c2ae38a3
David Pinney <david@pinney.org>
2022-05-13 12:40:19 -0400

    fix for data dir being left out

--- setup.py
+++ setup.py
@@ -3,34 +3,13 @@ Install wiires.
 '''
 
 from setuptools import setup
-from setuptools.command.develop import develop
-from setuptools.command.install import install
-from subprocess import check_call
-
-def pre_install():
-    check_call("pip install git+https://github.com/wind-python/windpowerlib.git@60f58ce76555b3ff33b31a959cd14958546b24e8".split())
-    check_call("pip install git+https://github.com/oemof/feedinlib.git@cd5d2392b398953e40a38909ea0f3a94986cb632".split())
-    check_call("pip install OpenDSSDirect.py[extras]".split())
-    check_call("pip install scipy".split())
-    check_call("pip install fire".split())
-    check_call("pip install matplotlib".split())
-    check_call("pip install plotly".split())
-
-class PreDevelopCommand(develop):
-    def run(self):
-        pre_install()
-        develop.run(self)
-
-class PreInstallCommand(install):
-    def run(self):
-        pre_install()
-        install.run(self)
 
 setup(
 	name='wiires',
 	version='1.0.0',
-	py_modules=['LCEM','dss_manipulation','graph_dss','scipy_optimize','wiires'],
+	py_modules=['wiires'],
 	install_requires = open("requirements.txt").readlines(),
+	include_package_data=True,
 	cmdclass={
 		'develop':PreDevelopCommand,
 		'install':PreInstallCommand

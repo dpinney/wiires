850b4116e7496d20f8215e54a2f44ffdc2077ddb
David Pinney <david@pinney.org>
2022-05-13 12:48:27 -0400

    fixing install

--- setup.py
+++ setup.py
@@ -7,7 +7,6 @@ from setuptools import setup
 setup(
 	name='wiires',
 	version='1.0.0',
-	py_modules=['wiires'],
 	packages=['wiires'],
 	install_requires = open("requirements.txt").readlines(),
 	include_package_data=True
--- wiires.py
+++
@@ -1,6 +0,0 @@
-import LCEM
-import dss_manipulation
-import graph_dss
-import scipy_test
-import OEM
-import hosting_cap
